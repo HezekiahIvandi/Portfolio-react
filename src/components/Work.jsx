0b825bd899b56397ff0b6cb9b315d470f8154567
unknown <hezekiahivandi@gmail.com>
2024-11-29 17:45:21 +0700

    Adding projects

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,30 +1,32 @@
 import React from 'react'
 import WorkImg from '../assets/workImg.jpeg';
 import realEstate from '../assets/realestate.jpg';
-
-const Card =()=>(
-    <>
-    <div style={{backgroundImage: `url(${WorkImg})`}} className='group shadow-lg shadow-[#040c16] container rounded-md flex flex-col justify-center items-center mx-auto content-div'>
-
-<div className='opacity-0 group-hover:opacity-100'>
-
-
-   
-       <div className='text-4xl font-bold block'>
-           React JS Application
-       </div>
-
-       <div className='flex justify-center pr-[1rem] pt-[2rem]'>
-           <a><button className='hover:bg-white hover:text-black mr-4 text-center items-center border-2 rounded-lg px-3 py-2'>Demo</button></a>
-           <a><button className='hover:bg-white hover:text-black text-center items-center border-2 rounded-lg px-3 py-2'>Code</button></a>
-      </div>
-
-  </div> 
- 
-
-</div>
-    </>
+import SatriaNFT from '../assets/satriaNFT.jpg'
+import PlayNPizza from "../assets/PlayNPizza.jpeg"
+import Taskify from "../assets/Taskify.jpeg"
+import HelloGram from "../assets/HelloGram.jpeg"
+import LMS from "../assets/LMS.jpeg"
+const Card = ({ img, title, code, demo }) => (
+    <div className='group shadow-lg shadow-[#040c16] container rounded-md flex flex-col justify-center items-center mx-auto overflow-hidden relative h-64'>
+        <img 
+            src={img} 
+            alt={title}
+            className='w-full h-full object-contain' 
+        />
+        <div className='opacity-0 group-hover:opacity-100 absolute inset-0 w-full h-full flex flex-col justify-center items-center bg-gradient-to-t from-black/80 to-black/80'>
+            <div className='text-3xl font-bold  text-white'>
+                {title}
+            </div>
+            <div className='flex justify-center pt-4'>
+                {demo?  <a href={demo} target='_blank'><button className='hover:bg-white hover:text-black mr-4 text-center items-center border-2 rounded-lg px-3 py-2 text-white'>Demo</button></a> : ""}
+                <a href={code} target='_blank'><button className='hover:bg-white hover:text-black text-center items-center border-2 rounded-lg px-3 py-2 text-white'>Code</button></a>
+            </div>
+        </div>
+    </div>
 )
+
+
+
 const Work = () => {
     return (
         <div name='work' className='px-[2rem] md:px-[4rem] lg:px-[10rem] pb-[20rem] pt-[6rem] mt-[10rem]'>
@@ -36,12 +38,12 @@ const Work = () => {
 
             <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-4'>
 
-                <Card />
-                <Card />
-                <Card />
-                <Card />
-                <Card/>
-                <Card />
+                <Card img={SatriaNFT} title ="SatriaNFT" code="https://github.com/HezekiahIvandi/SatriaNFT" demo="https://hezekiahivandi.github.io/SatriaNFT/"/>
+                
+                <Card img={Taskify} title ="Taskify" code="https://github.com/HezekiahIvandi/Taskify-Task-Management-App"/>
+                <Card img={LMS} title ="Wikimedia-LMS" code="https://github.com/HezekiahIvandi/LMS-Belajar-Daring-Wikimedia-Indonesia"/>
+                <Card img={HelloGram} title ="HelloGram" code="https://github.com/HezekiahIvandi/HelloGram"/>
+                <Card img={PlayNPizza} title ="Play & Pizza" code="https://github.com/HezekiahIvandi/Play-and-Pizza"/>
                 
             </div>
 
